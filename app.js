58f3c2bb3d0496ddf554dd2f38b0f0aef210343c
jaykumar9022 <jaykumarr9022@gmail.com>
2025-09-01 09:59:35 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { WebView } from "react-native-webview";
+
+export default function App() {
+  return (
+    <WebView
+      source={{ uri: "https://navya-ai.vercel.app/" }}
+      style={{ flex: 1 }}
+    />
+  );
+}
